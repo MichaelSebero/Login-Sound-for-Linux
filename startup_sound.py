144fd327a76a3444d20dfa7f661658a27fb6c7b6
Michael Sebero <106393611+MichaelSebero@users.noreply.github.com>
2022-11-04 10:58:37 -0400

    Update startup_sound.py

--- startup_sound.py
+++ startup_sound.py
@@ -1,5 +1,5 @@
 from pydub import AudioSegment
 from pydub.playback import play
 
-song = AudioSegment.from_wav("/-CoolRune-/System_Sounds/login.wav")
+song = AudioSegment.from_wav("/-CoolRune-/System_Sounds/Login.wav")
 play(song)
